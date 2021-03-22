2fe5b67404737110bdefe6f1b575cb7178b6dd88
Mateusz Kojro <mateuszkojro@outlook.com>
2021-03-22 09:57:55 +0100

    Some comments in Renderer.h

--- Renderer.h
+++ Renderer.h
@@ -5,12 +5,14 @@
 #ifndef GAME_OF_LIFE_RENDERER_H
 #define GAME_OF_LIFE_RENDERER_H
 
+/// \brief Struct containing coordinates of difrent objects
 struct Coord {
     int x;
     int y;
 };
 
-enum class Color{
+/// \brief Colors used in renderer
+enum class Color {
     Red,
     Green,
     Blue,
@@ -18,10 +20,24 @@ enum class Color{
     White
 };
 
+/// \brief Basic base class for all renderers.
 class Renderer {
 public:
-    virtual void draw_square(const Coord& position, int size_x, int size_y, const Color& fill) = 0;
-    virtual void clear_screen(const Color& fill) = 0;
+    /// \brief Creates window of given size
+    /// \param int Size in x dimention
+    /// \param int Size in y dimention
+    virtual void create_window(int size_x, int size_y) = 0;
+
+    /// \brief Draws a square on position wit size and fill
+    /// \param Coord position
+    /// \param size in x axis
+    /// \param size in y axis
+    /// \param Color color to fill square with
+    virtual void draw_square(const Coord &position, int size_x, int size_y, const Color &fill) = 0;
+
+    /// \brief Fill all screan with defined color
+    /// \param Color color to fill the screen with
+    virtual void clear_screen(const Color &fill) = 0;
 };
 
 
