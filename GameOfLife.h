7369f06221c1afa60faa54c45b47e678c542d665
Mateusz Kojro <mateuszkojro@outlook.com>
2021-03-25 09:05:20 +0100

    Some more comments

--- GameOfLife.h
+++ GameOfLife.h
@@ -7,6 +7,7 @@
 
 #include "GameEngine.h"
 
+/// \brief Implementation of the game of life
 class GameOfLife : public GameEngine {
 public:
     explicit GameOfLife(const Config &config)
@@ -22,12 +23,24 @@ public:
 
 private:
     /// \brief Gets value at position from current board
+    /// \param x x coordinate of position to get
+    /// \param y y coordinate of position to get
+    /// \return value at position in current_board_
     bool get_element(int x, int y) const;
+
     /// \brief Sets value at position on next_board
+    /// \param x x coordinate of position to be set
+    /// \param y y coordinate of position to be set
+    /// \param value value to set at position
     void set_element(int x, int y, bool value);
+
     /// \brief Swaps current_board with next_board to be shown and to prepare for next tick
     void swap_boards();
+
     /// \brief Counts a number of alive cells around cell at position
+    /// \param x x coordinate of analyzed position
+    /// \param y y coordinate of analyzed position
+    /// \return number of alive cells around position
     unsigned count_alive_near_position(int x, int y) const;
 
     /// \brief board that is currently shown on the screen
