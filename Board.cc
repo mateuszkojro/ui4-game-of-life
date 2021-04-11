f0352954acd020d824208021268ce9d5342ae1f7
Mateusz Kojro <mateuszkojro@outlook.com>
2021-04-11 21:23:19 +0200

    Some implementations in Board

--- Board.cc
+++ Board.cc
@@ -4,9 +4,16 @@
 
 #include "Board.h"
 
-Board::Board(bool *board) : board_(board) {}
+Board::Board(bool *board, size_t x, size_t y) :
+        size_x_(x),
+        size_y_(y) {
+    board_ = new bool[size()];
+    memcpy(board_, board, size());
+}
 
-Board::~Board() {}
+Board::~Board() {
+    delete[] board_;
+}
 
 size_t Board::get_size_x() const {
     return size_x_;
@@ -21,37 +28,57 @@ bool *Board::get_board() const {
 }
 
 bool &Board::operator()(int x, int y) {
-    return <#initializer#>;
+    return board_[translate_adress(x, y)];
 }
 
 std::array<bool *, 9> Board::get_neighbours(int x, int y) {
+    return get_neighbours(translate_adress(x, y));
+}
+
+std::array<bool *, 9> Board::get_neighbours(int i) {
     return std::array<bool *, 9>();
 }
 
-Board Board::load_board(const std::string path) {
-    return Board();
+Board Board::load_board(const std::string &path) {
 }
 
 void Board::save_board(const Board &) {
 
 }
 
-Board::Board(bool *board, size_t x, size_t y) {
-
+Board::Board(const Board &other) {
+    copy(other);
 }
 
-Board::Board(const Board &other) {
-
+void Board::copy(const Board &other) {
+    size_y_ = other.size_y_;
+    size_x_ = other.size_x_;
+    board_ = new bool[size()];
+    memcpy(board_, other.board_, size());
 }
 
 Board::Board(Board &&other) {
-
+    size_y_ = other.size_y_;
+    size_x_ = other.size_x_;
+    board_ = other.board_;
+    other.board_ = nullptr;
 }
 
-const Board &Board::operator=(const Board &) {
-    return <#initializer#>;
+const Board &Board::operator=(const Board &other) {
+    if (this == &other)
+        return *this;
+    copy(other);
+    return *this;
 }
 
-size_t Board::translate_adress(int x, int y) {
-    return 0;
+int Board::size() const {
+    return size_x_ * size_y_;
+}
+
+size_t Board::translate_adress(int x, int y) const {
+    return y * size_y_ + x;
+}
+
+bool &Board::operator()(int i) {
+    return board_[i];
 }
--- Board.h
+++ Board.h
@@ -6,6 +6,7 @@
 #define GAME_OF_LIFE_BOARD_H
 
 #include <array>
+#include <string>
 
 class Board {
 public:
@@ -17,14 +18,16 @@ public:
     /// \param y y coordinate
     /// \return reference to given field
     bool &operator()(int x, int y);
+    bool &operator()(int i);
 
     /// Rerturns array of ptrs to neighbours Some might be null
     /// \param x x coordinate
     /// \param y y coordinare
     /// \return array of ptrs to neighbour cells (some might be null)
     std::array<bool *, 9> get_neighbours(int x, int y);
+    std::array<bool *, 9> get_neighbours(int i);
 
-    static Board load_board(const std::string path);
+    static Board load_board(const std::string &path);
 
     static void save_board(const Board &);
 
@@ -46,6 +49,8 @@ public:
 
     const Board &operator=(const Board &);
 
+    int size() const;
+
     virtual ~Board();
 
 
@@ -54,7 +59,8 @@ private:
     /// \param x x coordinate
     /// \param y y coordinate
     /// \return addres in underlying arr
-    size_t translate_adress(int x, int y);
+    size_t translate_adress(int x, int y) const;
+    void copy(const Board& other);
 
     size_t size_x_;
     size_t size_y_;
